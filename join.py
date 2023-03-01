7e316ad70c03a37b146bff235f50a4806f1a8c10
nguyenvanvutlv <nguyenvanvu.tlv@gmail.com>
2023-03-01 22:02:22 +0700

    Cross, inner join

--- join.py
+++ join.py
@@ -32,16 +32,55 @@ VALUES ('Rafferty', 31),
        ('Smith', 34),
        ('Williams', NULL);
 """
-
-
-
 data = sqlite3.connect("data.db")
-
 myCursor = data.cursor()
+# myCursor.execute(department)
+# myCursor.execute(employee)
+# myCursor.execute(add_data_1)
+# myCursor.execute(add_data_2)
+# data.commit()
 
-myCursor.execute(department)
-myCursor.execute(employee)
-myCursor.execute(add_data_1)
-myCursor.execute(add_data_2)
-data.commit()
-# data.cursor().execute(add_data)
+
+# Cross join
+# kết hợp mỗi hàng từ bảng 1 với mỗi hàng ở bảng 2
+
+# kết hợp chéo rõ ràng
+# results = myCursor.execute("SELECT * FROM employee CROSS JOIN department").fetchall()
+# for result in results:
+#     print(result)
+
+
+# results = myCursor.execute("SELECT * FROM employee, department").fetchall()
+# for result in results:
+#     print(result)
+
+
+
+# Inner join -> kết hợp các giá trị của hai bảng
+# Yêu cầu mỗi hàng trong 2 bảng phải có cùng một giá trị cột
+# -> không được coi là lựa chọn tốt nhất trong các tình huống
+# so sánh từng hàng của A với từng hàng của B để tìm các cặp thoả mãn
+# có hai cách một là explicit [rõ ràng], implicit [ngầm] <không khuyến khích>
+# được dùng khi các cột giá trị của các bảng không có giá trị NULL
+def inner_join(database, explicit: bool = True, implicit: bool = False, is_null: bool = False):
+    assert explicit != implicit
+    cursor = database.cursor()
+    if explicit:
+        results = cursor.execute("""
+            SELECT employee.LastName, employee.DepartmentID, department.DepartmentName 
+            FROM employee
+            INNER JOIN department ON
+            employee.DepartmentID = department.DepartmentID;
+        """).fetchall()
+        print("Results of Explicit")
+        for result in results:
+            print(result)
+    else:
+        results = cursor.execute("""
+            SELECT employee.LastName, employee.DepartmentID, department.DepartmentName 
+            FROM employee, department
+            WHERE employee.DepartmentID = department.DepartmentID;
+        """).fetchall()
+        print("Results of Implicit")
+        for result in results:
+            print(result)
